5dc714c8f112448354edcc83eed8bd3bd67b4287
hoopma <hoopma@yandex.ru>
2024-07-19 15:08:51 +0300

    настроил celery, сделал пробную таску

--- web/web/settings.py
+++ web/web/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
     'rest_framework',
+    'django_celery_results',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +133,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:16379/0'
+
+CELERY_RESULT_BACKEND = 'django-db'
+
+CACHES = {
+   "default": {
+       "BACKEND": "django.core.cache.backends.redis.RedisCache",
+       "LOCATION": "redis://127.0.0.1:16379/1",
+   }
+}
+
+CELERY_CACHE_BACKEND = 'default'
+
