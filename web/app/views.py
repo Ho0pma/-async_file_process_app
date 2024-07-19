5d2742c21c70a2c9602cbe7150726c46620067e5
hoopma <hoopma@yandex.ru>
2024-07-19 12:49:56 +0300

    создал сериализатор, viewset для app, установил media_root, создал шаблон upload_form, настроил template_dirs, создал url files и files/upload

--- web/app/views.py
+++ web/app/views.py
@@ -1,3 +1,36 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.decorators import action
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .models import File
+from .serializers import FileSerializer
+
+
+class FileViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+
+    @action(detail=False, methods=['post', 'get'])
+    def upload(self, request):
+        if request.method == 'POST':
+            files = request.FILES.getlist('file')
+
+            for uploaded_file in files:
+                # Создаем и сохраняем объект File для каждого файла
+                file_obj = File(file=uploaded_file)
+                file_obj.save()
+
+            return HttpResponseRedirect('/files/')
+
+
+        elif request.method == 'GET':
+            # Возвращаем HTML-форму для загрузки файлов
+            return render(request, 'app/upload_form.html')
--- web/web/settings.py
+++ web/web/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
